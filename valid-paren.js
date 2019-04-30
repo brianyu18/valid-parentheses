2e2b52bc96fa17125f3b4b4210ef3f7caee8becc
brianyu18 <brianyu1990@gmail.com>
2019-04-30 11:48:03 -0400

    set checks for valid parentheses

--- valid-paren.js
+++ valid-paren.js
@@ -6,7 +6,12 @@ var isValid = function(s) {
    }
 
     let stack = []
-
+    for(let i = 0; i<s.length; i++){
+        var check = s[i]
+        if(hash[check]){
+            stack.push(hash[check])
+        } 
+    }
     if(stack.length !== 0){
         return false
     } else {
